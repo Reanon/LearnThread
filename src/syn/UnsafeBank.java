c6a5543bb4a1627ab546876c4c46b30913bdef92
reanon <793584285@qq.com>
2020-11-10 15:59:12 +0800

    :sparkles: 更新README文档，同时更新例子

--- src/syn/UnsafeBank.java
+++ src/syn/UnsafeBank.java
@@ -12,4 +12,60 @@ package syn;
 
 
 public class UnsafeBank {
+    public static void main(String[] args) {
+        Account account = new Account(100,"基金");
+        Drawing you = new Drawing(account,50,"you");
+        Drawing wo = new Drawing(account,100,"wo");
+
+        you.start();
+        wo.start();
+    }
+}
+
+//账户
+class Account {
+    // 余额
+    int money;
+    // 卡名
+    String name;
+
+    public Account(int money, String name) {
+        this.money = money;
+        this.name = name;
+    }
+}
+
+//银行：模拟取款
+class Drawing extends Thread {
+    Account account;
+    int drawingMoney;
+    int nowMoney;
+
+    public Drawing(Account account, int drawingMoney, String name) {
+        // 线程名
+        super(name);
+        this.account = account;
+        this.drawingMoney = drawingMoney;
+    }
+    //取钱
+    @Override
+    public void run() {
+        // 判断有没有钱
+        if (account.money - drawingMoney < 0) {
+            System.out.println(Thread.currentThread().getName() + "钱不够，取不了");
+        }
+        // sleep放大问题发生性
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        // 卡内余额 = 余额 - 取的钱
+        account.money = account.money - drawingMoney;
+        // 手里的钱
+        nowMoney = nowMoney + drawingMoney;
+        System.out.println(account.name + "余额为：" + account.money);
+        // Thread.currentThread().getName() = this.getName()
+        System.out.println(this.getName() + "手里的钱为：" + nowMoney);
+    }
 }
--- src/syn/UnsafeList.java
+++ src/syn/UnsafeList.java
@@ -11,5 +11,19 @@
 package syn;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class UnsafeList {
+    public static void main(String[] args) throws InterruptedException {
+        List<String> list = new ArrayList<String>();
+        // 循环开启10000个线程
+        for (int i = 0; i < 10000; i++) {
+            new Thread(()->{
+                list.add(Thread.currentThread().getName());
+            }).start();
+        }
+        Thread.sleep(3000);
+        System.out.println(list.size());
+    }
 }
